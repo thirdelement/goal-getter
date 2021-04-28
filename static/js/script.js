084bdacae9bdc83db96733835e70376c2b9ade04
thirdelement <gamechanger@Hotmail.co.uk>
2021-04-28 17:40:35 +0000

    feat: Add goal edit/done buttons

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,12 @@
-
-$(document).ready( function() {
-//Datepicker from https://jqueryui.com/datepicker/
-    $( "#target_date" ).datepicker(
-        {
-            dateFormat: "dd MM, yy"
-          }
-    );
-//Accordion from https://jqueryui.com/accordion/
-      $( "#accordion" ).accordion(
-        {
-          collapsible: true,
-          active: false
-        }
-      );
-  } );
-
-  
+$(document).ready(function () {
+  //Datepicker from https://jqueryui.com/datepicker/
+  $("#target_date").datepicker({
+    dateFormat: "dd MM, yy"
+  });
+  //Accordion from https://jqueryui.com/accordion/
+  $("#accordion").accordion({
+    collapsible: true,
+    active: false,
+    header: "h4"
+  })
+});
