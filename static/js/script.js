7eb811d06f741f0259c8baeb9b6f3d38994daf92
thirdelement <gamechanger@Hotmail.co.uk>
2021-04-28 16:04:58 +0000

    fix: Update to jQuery accordion as items not collapsing individually.

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,18 @@
-//Datepicker from https://jqueryui.com/datepicker/
+
 $(document).ready( function() {
+//Datepicker from https://jqueryui.com/datepicker/
     $( "#target_date" ).datepicker(
         {
             dateFormat: "dd MM, yy"
           }
     );
+//Accordion from https://jqueryui.com/accordion/
+      $( "#accordion" ).accordion(
+        {
+          collapsible: true,
+          active: false
+        }
+      );
   } );
 
   
