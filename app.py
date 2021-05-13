938bcb1845423fc3fe519ce612ee422b91ac3edd
thirdelement <gamechanger@Hotmail.co.uk>
2021-05-13 05:52:19 +0000

    feat: Add search functionality on goals.html.

--- app.py
+++ app.py
@@ -25,6 +25,13 @@ def get_goals():
     return render_template("goals.html", goals=goals)
 
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    goals = list(mongo.db.goals.find({"$text": {"$search": query}}))
+    return render_template("goals.html", goals=goals)
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
