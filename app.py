a74b08d852ffa76d6aa3c8736a44a5d55747e5ec
thirdelement <gamechanger@Hotmail.co.uk>
2021-05-21 17:38:05 +0000

    style: Add capitalization for username for flash Welcome message.

--- app.py
+++ app.py
@@ -77,7 +77,8 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
-                flash("Welcome, {}".format(request.form.get("username")))
+                flash("Welcome, {}".format(
+                    request.form.get("username").capitalize()))
                 return redirect(url_for("profile", username=session["user"]))
             else:
                 #invalid password match
