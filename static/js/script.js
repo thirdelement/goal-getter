98a4ae6929dbf82e6c9c65d6f376fb7e2847ad50
thirdelement <gamechanger@Hotmail.co.uk>
2021-05-25 18:09:10 +0000

    feat: Add JS and style for form field validation message.

--- static/js/script.js
+++ static/js/script.js
@@ -5,17 +5,42 @@ $(document).ready(function () {
   });
 
   //Bootstrap tab buttons from https://getbootstrap.com/docs/4.6/components/navs/#javascript-behavior
-  $(".button-goal").click(function(){
+  $(".button-goal").click(function () {
     $('#nav-goal-tab').tab('show')
   });
-  $(".button-reality").click(function(){
+  $(".button-reality").click(function () {
     $('#nav-reality-tab').tab('show')
   });
-  $(".button-options").click(function(){
+  $(".button-options").click(function () {
     $('#nav-options-tab').tab('show')
   });
-  $(".button-wayforward").click(function(){
+  $(".button-wayforward").click(function () {
     $('#nav-wayforward-tab').tab('show')
   });
-  
+
+  //Add validity message from https://stackoverflow.com/questions/32829776/setting-custom-html5-validity-message-property-ignores-pattern-regex
+  $('textarea, input').on('input', function () {
+    this.setCustomValidity("");
+    var valid = $(this).get(0).reportValidity();
+    if (!valid)
+      this.setCustomValidity('Enter between 5 to 15 characters (letters or numbers)');
+    else
+      return 1;
+  });
+
+  //Check add_goal form for validity and display message.  Credit: https://stackoverflow.com/questions/45789010/how-to-use-html-form-checkvalidity/45789752
+  $("#submit-add_goal").click(function (action) {
+    var forms = document.getElementById('form-add_goal')
+    if (!forms.checkValidity()) {
+      document.getElementById('alert-options').style.display = 'block';
+    }
+  });
+
+  //Check edit_goal form for validity and display message.
+  $("#submit-edit_goal").click(function (action) {
+    var forms = document.getElementById('form-edit_goal')
+    if (!forms.checkValidity()) {
+      document.getElementById('alert-wayforward').style.display = 'block';
+    }
+  });
 });
