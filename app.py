416ec8c4c0a253d2da89f23943a511d75dc0cc5b
thirdelement <gamechanger@Hotmail.co.uk>
2021-06-04 17:26:48 +0000

    ci: Change debug from True to False in app.py for production.

--- app.py
+++ app.py
@@ -403,5 +403,5 @@ def not_found(error):
 if __name__ == "__main__":                  
     app.run(host=os.environ.get("IP"),      
             port=int(os.environ.get("PORT")), 
-            debug=True)                     
+            debug=False)                     
     # set debug to True so we can see errors.  Set to false at go live
